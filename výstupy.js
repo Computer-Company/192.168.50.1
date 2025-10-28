51cf561b5f4be638add41356ff9e74429ad733cc
Computer-Company <podpora.cc@outlook.cz>
2025-10-28 15:38:47 +0100

    Update výstupy.js

--- výstupy.js
+++ výstupy.js
@@ -3,8 +3,11 @@ console.log (vystupy_navic)
 
 port: (id=1 'CC Wi-Fi_5G' 6+VkP*S?rN!4C8%+5)
 port: (id=2 'Nevim' nevimjakyjeheslo)
+port: (id=3 'Naše' našewifi)
+port: (id=4 'Moje' mojewifi)
 
 end
+
 
 
 
