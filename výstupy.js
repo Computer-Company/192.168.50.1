c82ea161fcd33cff24406fe17be287376278f83b
Computer-Company <podpora.cc@outlook.cz>
2025-11-21 22:37:07 +0100

    Refactor vystupy_navic and add port details
    Refactor vystupy_navic function and add port configurations.

--- výstupy.js
+++ výstupy.js
@@ -1,8 +1,14 @@
 def (vystupy_navic)
 console.log (vystupy_navic)
 
+port: (id=1 'CC Wi-Fi_5G' 6+VkP*S?rN!4C8%+5' )
+port: (id:7=2 'Nevim' nevimjakyjeheslo)
+port: (id:8=3 'Naše' našewifi)
+port: (id:6=4 'Moje' mojewifi)
+
 
 end
+
 
 
 
