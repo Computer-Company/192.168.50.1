4171b178f2e320ebda691252850c8714157ad9c5
Computer-Company <podpora.cc@outlook.cz>
2025-11-21 22:02:41 +0100

    Update výstupy.js

--- výstupy.js
+++ výstupy.js
@@ -1,12 +1,9 @@
 def (vystupy_navic)
 console.log (vystupy_navic)
 
-port: (id=1 'Moje_Wi-Fi' 8M@r=e?5Zf!A@P4?+!)
-port: (id:7=2 'Nevim' nevimjakyjeheslo)
-port: (id:8=3 'Naše' našewifi)
-port: (id:6=4 'Moje' mojewifi)
 
 end
+
 
 
 
