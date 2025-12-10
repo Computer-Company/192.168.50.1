2a2e661a168b5ad723b7e7f2137f39333a51ecc6
Computer-Company <podpora.cc@outlook.cz>
2025-12-10 21:13:12 +0100

    Update router.js

--- router.js
+++ router.js
@@ -31,10 +31,9 @@ off [1,2,3,4,5] // zablokuje porty LAN 1,2,3,4,5 na routeru
 
 console.log (MAC_adresy)
 
-enabled [cc-e6-86-6f-9d-d6,60-D8-19-27-2D-9E, 6c-55-63-9c-f5-0c] // enabled - povolí zadané MAC adresy
-//Adam   14-20-20-04-cb-37 : mobil Galaxy A14_jeho hlavní,  58-20-71-37-ca-05 : mobil Redmi A5, pt-26-89-fh-gd-00 :  notebook lenovo-(červené)-DESKTOP-PTL6GS8
-//Adam   14-01-52-b0-51-c9 : starý mobil Galaxy A20e,  00-24-2b-04-7d-32 : notebook ACER 1 (prý ten malý),  
-//Já     CC-E6-86-6F-9D-D6 : Můj mobil A05s,    60-D8-19-27-2D-9E : Můj Acer1 (červený)
+enabled [cc-e6-86-6f-9d-d6,60-D8-19-27-2D-9E, 6c-55-63-9c-f5-0c, F0-03-8C-84-68-E5] // enabled - povolí zadané MAC adresy
+
+//Já     CC-E6-86-6F-9D-D6 : Můj mobil A05s,    60-D8-19-27-2D-9E : Můj Acer1 (červený),     F0-03-8C-84-68-E5 : Můj Asus (bílý)
 //tatínek  6c-55-63-9c-f5-0c : jeho mobil 
 
 disable [Disabled-All-Instanceof] // disable - zakáže všechny neuložené MAC adresy
@@ -47,6 +46,7 @@ decrypted [1,0] // zašifruje provoz a bude všem zobrazovat 1 a 0
 
 
 end
+
 
 
 
